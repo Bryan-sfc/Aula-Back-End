1704e54dd1400ffc99b9699ce6f04cf6da3f6f0a
Bryan Goncalves <goncalvesbryan424@gmail.com>
2024-10-24 16:48:55 -0300

    Aula c# Dia 24/10

---
+++ Arrays/Program.cs
@@ -0,0 +1,10 @@
+﻿string[] carros = new string[3];
+
+for (int i = 0; i <= 2; i++){
+    Console.WriteLine($"Digite o nome do carro a ser cadastrado:");
+    carros[i] = Console.ReadLine();
+}
+
+for (int i = 0; i <= 2; i++){
+    Console.WriteLine($"Carros cadastrados: {carros[i]}");
+}
---
+++ DesafioArray01/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] numeros = new int[6];
+int par = 0;
+int impar = 0;
+
+for (int i = 0; i < 6; i++){
+    Console.WriteLine($"Digite um Numero inteiro Positivo: ");
+    numeros[i] = int.Parse(Console.ReadLine());
+
+
+    if (numeros[1] % 2 == 0){
+        par++;
+    }
+    else{
+        impar++;
+    }
+}
+
+Console.WriteLine($"Quantidade total de numeros pares: {par}");
+Console.WriteLine($"Quantidade total de numeros impares: {impar}");
